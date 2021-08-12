c57f18383e0906111b138684feffd3dc6df97951
Jacob Hilker <jacob.hilker2@gmail.com>
2021-08-12 17:34:34 -0400

    updated index layout

--- assets/css/tailwind.config.js
+++ assets/css/tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-  purge: [],
+  purge: ['layouts/**/*.html'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {},
