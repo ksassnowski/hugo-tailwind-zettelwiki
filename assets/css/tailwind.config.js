0d57f48359520893d2bf1728746a2913eb155c9a
Jacob Hilker <jacob.hilker2@gmail.com>
2021-08-11 22:32:29 -0400

    initial commit

---
+++ assets/css/tailwind.config.js
@@ -0,0 +1,16 @@
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  purge: [],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {},
+    colors: {
+      teal: colors.teal
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
