9de706c94874f451d98b0a3aaa7c4c93c0bb8346
Jacob Hilker <jacob.hilker2@gmail.com>
2021-08-16 16:52:21 -0400

    deleted teal from colors in tailwind config

--- assets/css/tailwind.config.js
+++ assets/css/tailwind.config.js
@@ -1,14 +1,11 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-  //mode: 'jit',
+
   purge: ['layouts/**/*.html'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {},
-    colors: {
-      teal: colors.teal
-    },
   },
   variants: {
     extend: {},
